9975ec17559a8b1f1c69eaa03c85e367dac9fca4
Julius Hernandez <julius_hernandez@apple.com>
2023-08-26 21:39:15 -0700

    figuring stuff out

--- dash_app/app1.py
+++ dash_app/app1.py
@@ -3,11 +3,24 @@ from dash.dependencies import Input, Output
 import plotly
 
 app = Dash(__name__)
+
 app.layout = html.Div([
     html.H1('Hello Dash'),
     html.Div(id='output-div'),
-    dcc.Dropdown(),
+    dcc.Dropdown(
+        options=['Red', 'Blue', 'Green'],
+        id='input-dropdown',
+        value='Red'
+    ),
     dcc.Graph(id='output-graph')
 ])
 
-app.run(debug=True, port=8000)
+# @app.callback(Output(), Input())
+# def interactive_chart(input):
+#     return output
+
+
+if __name__ == '__main__':
+    app.run_server(port=8080, host='localhost', debug=True)
+
+# eof
